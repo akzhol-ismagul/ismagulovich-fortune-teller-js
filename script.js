b55a89b54d4029393aa20d7893c819fbdf024f30
akzhol omorov <kgakzhol@gmail.com>
2022-06-07 23:02:26 +0600

    fortune teller

---
+++ script.js
@@ -0,0 +1,4 @@
+function fortuneTeller (numOfKids, partnersName, geoLoc, jobTitle){
+    console.log("You will be a " + jobTitle + " in " + geoLoc + ", and married to " + partnersName + " with " + numOfKids + " kids.")
+}
+fortuneTeller(4, "Alex", "NY", "software developer")
